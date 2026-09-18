8a2058defd8a55ffc09b649067b6ff963e1c621f
Ajath Shatru <thangara005@mymail.sim.edu.sg>
2026-09-19 00:09:43 +0800

    Update NutriApp website

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request
 
+# Create the Flask application
 app = Flask(__name__)
 
 
+# Show the home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
+# Show the team members page
 @app.route("/team")
 def team():
+    # Team member details
     members = [
         {
             "name": "Chong Choong Zing Shaun",
@@ -31,17 +35,20 @@ def team():
     return render_template("team.html", members=members)
 
 
+# Show the documentation page
 @app.route("/documentation")
 def documentation():
     return render_template("docs.html")
 
 
+# Show and process the request form
 @app.route("/form", methods=["GET", "POST"])
 def form():
     action_type = request.args.get("type", "Contact Us")
     submitted = False
     name = ""
 
+    # Get the form details after submission
     if request.method == "POST":
         action_type = request.form.get("action_type", action_type)
         name = request.form.get("name", "")
@@ -55,5 +62,6 @@ def form():
     )
 
 
+# Run the website locally
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
